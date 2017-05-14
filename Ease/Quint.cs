c9a333e6b61948d3ec32a8adc8d866cb7f50cf7e
Jan-Ole Schümann <j.o.schuemann@gmx.de>
2017-05-14 12:01:50 +0200

    More easing functions

---
+++ Ease/Quint.cs
@@ -0,0 +1,29 @@
+﻿using TS;
+
+public class Quint
+{
+    public static TSEase.EaseFunction EaseIn = FEaseIn;
+    private static float FEaseIn(float t, float b, float c, float d)
+    {
+        t /= d;
+        return c*t*t*t*t*t + b;
+    }
+
+    public static TSEase.EaseFunction EaseOut = FEaseOut;
+    private static float FEaseOut(float t, float b, float c, float d)
+    {
+        t /= d;
+        t--;
+        return -c * (t*t*t*t*t - 1) + b;
+    }
+
+    public static TSEase.EaseFunction EaseInOut = FEaseInOut;
+    private static float FEaseInOut(float t, float b, float c, float d)
+    {
+
+        t /= d/2;
+        if (t < 1) return c/2*t*t*t*t*t + b;
+        t -= 2;
+        return -c/2 * (t*t*t*t*t - 2) + b;
+    }
+}
