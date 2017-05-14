2514483b197364901106d0b38e8fb9e69b09944d
Jan-Ole Schümann <j.o.schuemann@gmx.de>
2017-05-14 12:05:47 +0200

    More easing functions

---
+++ Ease/Circ.cs
@@ -0,0 +1,26 @@
+﻿using TS;
+using UnityEngine;
+
+public class Circ
+{
+    public static TSEase.EaseFunction EaseIn = FEaseIn;
+    private static float FEaseIn(float t, float b, float c, float d)
+    {
+        t /= d;
+        return -c * (Mathf.Sqrt(1 - t*t) - 1) + b;    }
+
+    public static TSEase.EaseFunction EaseOut = FEaseOut;
+    private static float FEaseOut(float t, float b, float c, float d)
+    {
+        t /= d;
+        t--;
+        return c * Mathf.Sqrt(1 - t*t) + b;    }
+
+    public static TSEase.EaseFunction EaseInOut = FEaseInOut;
+    private static float FEaseInOut(float t, float b, float c, float d)
+    {
+        t /= d/2;
+        if (t < 1) return -c/2 * (Mathf.Sqrt(1 - t*t) - 1) + b;
+        t -= 2;
+        return c/2 * (Mathf.Sqrt(1 - t*t) + 1) + b;    }
+}
---
+++ Ease/Expo.cs
@@ -0,0 +1,26 @@
+﻿using TS;
+using UnityEngine;
+
+public class Expo
+{
+    public static TSEase.EaseFunction EaseIn = FEaseIn;
+    private static float FEaseIn(float t, float b, float c, float d)
+    {
+        return c * Mathf.Pow( 2, 10 * (t/d - 1) ) + b;
+    }
+
+    public static TSEase.EaseFunction EaseOut = FEaseOut;
+    private static float FEaseOut(float t, float b, float c, float d)
+    {
+        return c * ( -Mathf.Pow( 2, -10 * t/d ) + 1 ) + b;
+    }
+
+    public static TSEase.EaseFunction EaseInOut = FEaseInOut;
+    private static float FEaseInOut(float t, float b, float c, float d)
+    {
+        t /= d/2;
+        if (t < 1) return c/2 * Mathf.Pow( 2, 10 * (t - 1) ) + b;
+        t--;
+        return c/2 * ( -Mathf.Pow( 2, -10 * t) + 2 ) + b;
+    }
+}
