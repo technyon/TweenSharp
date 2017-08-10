e61e160fc974795c92ccaf541a2dc1112e97fe8c
jschuemann <jschuemann@bigpoint.net>
2017-08-10 17:05:41 +0200

    fix Cubic ease

--- Ease/Cubic.cs
+++ Ease/Cubic.cs
@@ -5,25 +5,19 @@ public class Cubic
     public static TSEase.EaseFunction EaseIn = FEaseIn;
     private static float FEaseIn(float t, float b, float c, float d)
     {
-        t /= d;
-        return c*t*t*t + b;
+        return c*(t/=d)*t*t + b;
     }
 
     public static TSEase.EaseFunction EaseOut = FEaseOut;
     private static float FEaseOut(float t, float b, float c, float d)
     {
-        t /= d;
-        t--;
-        return -c * (t*t*t - 1) + b;
+        return c * ((t = t / d - 1) * t * t + 1) + b;
     }
 
     public static TSEase.EaseFunction EaseInOut = FEaseInOut;
     private static float FEaseInOut(float t, float b, float c, float d)
     {
-
-        t /= d/2;
-        if (t < 1) return c/2*t*t*t + b;
-        t -= 2;
-        return -c/2 * (t*t*t - 2) + b;
+        if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+        return c / 2 * ((t -= 2) * t * t + 2) + b;
     }
 }
