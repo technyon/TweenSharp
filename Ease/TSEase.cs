c4e7da858bbdff08f273dc8d776d966de2d1fda3
Ole S <j.o.schuemann@gmx.de>
2017-12-18 10:42:46 +0100

    Cleanup TSEase

--- Ease/TSEase.cs
+++ Ease/TSEase.cs
@@ -5,10 +5,5 @@ namespace TS
     public class TSEase
     {
         public delegate float EaseFunction(float t, float b, float c, float d);
-
-        public static float Linear(float t, float b, float c, float d)
-        {
-            return c * t / d + b;
-        }
     }
 }
