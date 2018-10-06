8c994e686615f7d377d329934c3b4b3cc505166f
Jan-Ole Schümann <j.o.schuemann@gmx.de>
2018-10-06 14:23:25 +0200

    optimize imports

--- Ease/TSEase.cs
+++ Ease/TSEase.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-
-namespace TS
+﻿namespace TS
 {
     public class TSEase
     {
--- Helpers/TSHelper.cs
+++ Helpers/TSHelper.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection;
-using UnityEngine;
 
 public static class TSHelper
 {
